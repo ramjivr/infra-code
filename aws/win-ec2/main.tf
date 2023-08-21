4469c6bdb2475c0f38780e5857607e5eeab90a06
ramjivr <102302461+ramjivr@users.noreply.github.com>
2023-08-21 15:16:40 +0100

    Update main.tf

--- aws/win-ec2/main.tf
+++ aws/win-ec2/main.tf
@@ -1,59 +1,13 @@
-##################################################################################
-# CONFIGURATION - added for Terraform 0.14
-##################################################################################
-
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~>3.0"
-    }
-  }
-}
-
-##################################################################################
-# PROVIDERS
-##################################################################################
-
-
-
 provider "aws" {
-  profile = "myprofile1"
-  region  = var.region
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "us-east-1"
 }
 
-
-##################################################################################
-# RESOURCES
-##################################################################################
-
-
-
-
-resource "aws_instance" "windows" {
-  
-  ami           = "ami-00cefd54ba36fdf42"
-  instance_type = "t2.micro"
-  associate_public_ip_address = true
-  key_name = "winkey"
-  vpc_security_group_ids = ["sg-23847238474f"]
-  subnet_id     = "subnet-2348273423"
-
-  tags = {
-    Name = var.instance_names
-
+resource "aws_instance" "ec2_instance" {
+    ami = "${var.ami_id}"
+    count = "${var.number_of_instances}"
+    subnet_id = "${var.subnet_id}"
+    instance_type = "${var.instance_type}"
+    key_name = "${var.ami_key_pair_name}"
 } 
-
-}
-
-
-
-##################################################################################
-# OUTPUT
-##################################################################################
-
-output "aws_instance_public_dns" {
-  value = aws_instance.windows.public_dns
-}
-
